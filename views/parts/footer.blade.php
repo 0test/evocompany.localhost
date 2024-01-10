96612fbb987ef8fb3820d0285e1943c46e0078aa
Andrey <0test@mail.ru>
2024-01-10 13:48:03 +0300

    add template

---
+++ views/parts/footer.blade.php
@@ -0,0 +1,14 @@
+<footer class="bg-dark py-4 mt-auto">
+	<div class="container px-5">
+		<div class="row align-items-center justify-content-between flex-column flex-sm-row">
+			<div class="col-auto"><div class="small m-0 text-white">Copyright &copy; 2024</div></div>
+			<div class="col-auto">
+				<a class="link-light small" href="#!">Приватность</a>
+				<span class="text-white mx-1">&middot;</span>
+				<a class="link-light small" href="#!">Всякое</a>
+				<span class="text-white mx-1">&middot;</span>
+				<a class="link-light small" href="contact.html">Контакты</a>
+			</div>
+		</div>
+	</div>
+</footer>
---
+++ views/parts/footer_scripts.blade.php
@@ -0,0 +1,4 @@
+<!-- Bootstrap core JS-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Core theme JS-->
+<script src="template/js/scripts.js"></script>
